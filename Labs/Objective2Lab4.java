e05e554b443a702e4835c4e3978537fac60cf8ae
Rochelle <rocolorado@outlook.com>
2018-04-15 16:24:05 -0600

    complete Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,14 @@
+public class Objective2Lab4 {
+  public static void main( String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+    average = 7.66666666667;
+
+    System.out.println(+ average );
+
+
+  }
+}
