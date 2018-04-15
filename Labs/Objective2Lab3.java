1e430612953fd7ce8b41b28d0a425a35714142a4
Rochelle <rocolorado@outlook.com>
2018-04-15 15:54:12 -0600

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String [] args ){
+    String food1, food2, food3;
+
+    food1 = "Apple";
+    food2 = "Banana";
+    food3 = "Coconut";
+
+    System.out.println("My Favorite Foods Are:\n1." + food1 + "\n2." + food2 + "\n3." + food3);
+
+  }
+}
