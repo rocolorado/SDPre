dc89e0a30080f3a4006836232fb925bd364c3286
Rochelle <rocolorado@outlook.com>
2018-04-15 14:18:45 -0600

    with study drill modifications

--- JavaTheHardway/CreatingVariables.java
+++ JavaTheHardway/CreatingVariables.java
@@ -1,27 +1,40 @@
 public class CreatingVariables {
   public static void main( String[] args ){
-    int x, y, age;
-    double seconds, e, checking;
-    String firstName, lastName, title;
+    int x, y, age, r;
+    double seconds, e, checking, weight;
+    String firstName, lastName, title, dogName1, dogName2, dogLastName, dogType;
+
 
     x = 10;
     y= 400;
     age = 39;
+    r = 3;
 
     seconds = 4.71;
     e = 2.71828182845904523536;
     checking = 1.89;
+    weight = 35.5;
 
     firstName = "Graham";
     lastName = "Mitchell";
     title = "Mr.";
+    dogName1 = "Twinkie";
+    dogName2 = "Peanut";
+    dogLastName = "Peck";
+    dogType = "Corgi";
 
     System.out.println( "The variable x contains " + x );
+    System.out.println( "The varialble r contains " + r );
     System.out.println( "The value " + y + " is stored in the variable y." );
     System.out.println( "The experiment took " + seconds + " seconds." );
+    System.out.println( "Twinkie weighs " + weight + "pounds." );
+    System.out.println( "My dog's names are " + dogName1 + " and " + dogName2 + " " + dogLastName + "." );
+    System.out.println( dogName1 + " and " + dogName2 + " are " + dogType + "." );
     System.out.println( "A favorite irrational # is Euler's number: " + e );
     System.out.println( "Hopefully you have more than $" + checking + "!" );
     System.out.println( "My name's " + title + " " + firstName + lastName );
+
+    //For studydrill I added intiger, double and two strings //
 
   }
 }
