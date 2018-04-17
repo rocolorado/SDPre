638bee96300bab0efebc7f213cae5ae1800d7d4a
Rochelle <rocolorado@outlook.com>
2018-04-17 17:47:42 -0600

    completed Objective4Lab1

--- Labs/Objective4Lab1.java
+++ Labs/Objective4Lab1.java
@@ -3,12 +3,12 @@ public class Objective4Lab1 {
     int num1 = 5;
     int num2 = 15;
 
-    if (num1 < num2){
-      System.out.println(num2 + " is greater than " + num1 );
+    if (num1 > num2){
+      System.out.println(num1 + " is greater than " + num2 );
     }
-    else{
-      System.out.println(num2 + " is less than " + num1 );
-    }
+    else if (num2 > num1){
+       System.out.println(num2 + " is greater than " + num1 );
+
 
 
 
@@ -16,5 +16,6 @@ public class Objective4Lab1 {
 
         // output is - 15 is greater than 5//
         //determine if num1 is greater than or less than num2 and print out the findings//
+      }
     }
   }
