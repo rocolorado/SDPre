aa09204ead95b7144aa4864899eb8b4777a62aa3
Rochelle <rocolorado@outlook.com>
2018-04-17 14:24:51 -0600

    completed Exercise 17 with study drills

---
+++ JavaTheHardway/ClubBouncer.java
@@ -0,0 +1,19 @@
+public class ClubBouncer {
+  public static void main( String[] args){
+    int age = 22;
+    boolean onGuestList = false;
+    double allure = 7.5;
+    String gender = "F";
+
+    if ( onGuestList || age >= 21 || (gender.equals("F") && allure >= 8 ) ) {
+      System.out.println("You are allowed to enter the club." );
+    }
+
+    else {
+      System.out.println("You are not allowed to enter the club." );
+
+    }
+  }
+}
+//Study drill- I put a printline between lines 10 and 11 but it does not complile because the //
+//else statement needs to be after the if statement.//
