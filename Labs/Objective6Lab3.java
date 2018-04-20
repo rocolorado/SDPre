67bde48199a9808afea083e60f7c8bb4dd67472b
Rochelle <rocolorado@outlook.com>
2018-04-20 13:02:12 -0600

    Completed Objective6Lab3

---
+++ Labs/Objective6Lab3.java
@@ -0,0 +1,16 @@
+public class Objective6Lab3 {
+  public static void main(String[] args){
+    int counter = 1;
+    while (counter <= 20) {
+      if ((counter % 2) == 0)  {
+        System.out.println( counter + " is even " );
+      }
+      else
+        System.out.println( counter + " is odd " );
+
+
+      counter = counter +1;
+  }
+  }
+} // print 1-20 while stating if the # is odd or even.  Write a while loop  and use counter  to have the loop execute 20 times
+//write a stataement that determines if the # is odd or even  ( 1 is odd.... )
