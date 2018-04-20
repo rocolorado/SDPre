5e6efe40df5f2df396d7ee7a0194eefc96e16fa2
Rochelle <rocolorado@outlook.com>
2018-04-20 13:28:08 -0600

    Completed Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,24 @@
+public class Objective6Lab4 {
+  public static void main(String[] args){
+
+    int  sum = 0, count = 0;
+    while (count <= 20) {
+      sum = (count + sum);
+      count++;
+
+
+
+
+}
+    System.out.println( sum );
+
+}
+
+}
+
+
+
+  //create a class structure with name Obj6L4
+  //create main method
+  //create two variables to store in the current sum total and a counter
+  //write a while loop that counts variable to execute to 20, increment the value with each iteration and update sum by adding count to sum
