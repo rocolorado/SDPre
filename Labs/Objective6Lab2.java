94b579f6388bc62a280a7309a770168f5b10bcb8
Rochelle <rocolorado@outlook.com>
2018-04-20 12:39:32 -0600

    Completed Objective6Lab2

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,11 @@
+public class Objective6Lab2 {
+  public static void main(String [] args){
+    int counter = 0;
+
+    while (counter <= 20) {
+      counter = counter +1;
+      System.out.println( counter);
+
+    }
+  }
+} //print value of counter with each ineration; add 1 to counter each iteration so your loop doesn't run forever.
